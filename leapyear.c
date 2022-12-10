5800412f708ead9418892f807391fdcee7f5985e
omguptalko <119674842+omguptalko@users.noreply.github.com>
2022-12-10 14:57:18 +0530

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,17 @@
+// Program to check leap year
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a  ;
+float ratio;
+printf(" entered year");
+scanf("%d",&a);
+if( a%4 ==0)
+
+printf("entered year is  leap year %d",a);
+else
+printf("entered year is not leap year %d",a);
+
+}
+ 
